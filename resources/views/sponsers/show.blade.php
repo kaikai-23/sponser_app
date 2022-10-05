a8d28ea314f8959c1065ea5775a75b031bad9df8
kaikai-23 <gate.sea0203@gmail.com>
2022-10-06 00:01:55 +0900

    編集機能の実装から

--- resources/views/sponsers/show.blade.php
+++ resources/views/sponsers/show.blade.php
@@ -10,8 +10,9 @@
             <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl">{{ $sponser->title }}</h2>
             <h3>{{ $sponser->user->name }}</h3>
             <p class="text-sm mb-2 md:text-base font-normal text-gray-600">
-                <span class="text-red-400 font-bold">{{ date('Y-m-d H:i:s', strtotime('-1 day')) < $sponser->created_at ? 'NEW' : '' }}</span>
-                {{ $sponser->created_at }}
+                <span class="text-red-400 font-bold">{{ date('Y-m-d H:i:s', strtotime('-1 day')) < $sponser->created_at ? 'NEW' : '' }}</span><br>
+                カテゴリー: {{ $sponser->category->name }} <br>
+                記事作成日: {{ $sponser->created_at }}
             </p>
             <img src="{{ $sponser->image_url}}" alt="" class="mb-4">
             <p class="text-gray-700 text-base">{!! nl2br(e($sponser->body)) !!}</p>
